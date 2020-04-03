b455155308f5c0b39501cdcd7e42718e19470c79
Chris Tieman <iam@christieman.com>
2020-04-03 16:04:51 -0400

    Updated Clock to use time format setting

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,18 +1,22 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
 import moment from 'moment';
+
+import { selectors as settingsSelectors } from 'modules/settings';
 
 const StyledClock = styled.div`
   font-size: 20px;
 `;
 
-const Clock = () => {
-  const [time, setTime] = useState(moment().format('HH:mm:ss'));
+const Clock = ({ settings }) => {
+  const formatTime = settings.formatTime === '12' ? 'hh:mm:ss A' : 'HH:mm:ss';
+  const [time, setTime] = useState(moment().format(formatTime));
 
   const onTick = useCallback(() => {
-    const time = moment().format('HH:mm:ss');
+    const time = moment().format(formatTime);
     setTime(time);
-  }, []);
+  }, [formatTime]);
 
   useEffect(() => {
     const tickInterval = setInterval(onTick, 200);
@@ -26,4 +30,8 @@ const Clock = () => {
   );
 };
 
-export default Clock;
+const mapState = state => ({
+  settings: settingsSelectors.all(state),
+});
+
+export default connect(mapState)(Clock);
